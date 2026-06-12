0c0c5db771d7d058a0044daddc32758e011b9e1f
Andres Lopez Esquivel <lopezesquivelandres1@gmail.com>
2026-06-11 22:09:23 -0600

    project(DummyProject): implement /api/continent/<continent-name> endpoint

--- DummyServer/server.js
+++ DummyServer/server.js
@@ -3,25 +3,39 @@ import { createServer } from 'node:http';
 
 const PORT = 8000;
 
+function sendError(res, status, error, message) {
+  res.setHeader('Content-Type', 'application/json');
+  res.statusCode = status;
+  res.end(JSON.stringify({ error, message }));
+}
+
 const server = createServer(async (req, res) => {
   if (req.url === '/api' && req.method === 'GET') {
     try {
       const data = await getDataFromDB();
-      const stringifiedJSON = JSON.stringify(data);
       res.setHeader('Content-Type', 'application/json');
       res.statusCode = 200;
-      res.end(stringifiedJSON);
+      res.end(JSON.stringify(data));
     } catch (err) {
-      res.writeHead(500);
-      res.end('Internal Server Error');
+      sendError(res, 500, 'Internal Server Error', 'An Internal Server Error occurred');
+    }
+  } else if (req.url.startsWith('/api/continent/') && req.method === 'GET') {
+    const continent = req.url.split('/').at(-1);
+    try {
+      const data = await getDataFromDB();
+      const continentData = data.filter(item => item.continent.toLowerCase() === continent.toLowerCase());
+      if (continentData.length) {
+        res.setHeader('Content-Type', 'application/json');
+        res.statusCode = 200;
+        res.end(JSON.stringify(continentData));
+      } else {
+        sendError(res, 404, 'Not Found', "Requested continent wasn't found");
+      }
+    } catch (err) {
+      sendError(res, 500, 'Internal Server Error', 'An Internal Server Error occurred');
     }
   } else {
-    res.setHeader('Content-Type', 'application/json');
-    res.statusCode = 404;
-    res.end(JSON.stringify({
-      error: "Not Found",
-      message: "Requested route doesn't exist"
-    }));
+    sendError(res, 404, 'Not Found', "Requested route doesn't exist");
   }
 });
 
